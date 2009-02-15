9536e722c54615b75b4e8f1644f6b3730719fe39
WebGeek <WebGeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-15 22:06:16 +0000

    Version 1.9.8.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-spamfree/trunk@94809 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-spamfree.php
+++ wp-spamfree.php
@@ -4,7 +4,7 @@ Plugin Name: WP-SpamFree
 Plugin URI: http://www.hybrid6.com/webgeek/plugins/wp-spamfree
 Description: An extremely powerful anti-spam plugin that virtually eliminates comment spam. Finally, you can enjoy a spam-free WordPress blog! Includes spam-free contact form feature as well.
 Author: Scott Allen, aka WebGeek
-Version: 1.9.8.2
+Version: 1.9.8.3
 Author URI: http://www.hybrid6.com/webgeek/
 */
 
@@ -25,10 +25,11 @@ Author URI: http://www.hybrid6.com/webgeek/
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+
 // Begin the Plugin
 
 function spamfree_init() {
-	$wpSpamFreeVer='1.9.8.2';
+	$wpSpamFreeVer='1.9.8.3';
 	update_option('wp_spamfree_version', $wpSpamFreeVer);
 	spamfree_update_keys(0);
 	}
@@ -297,7 +298,9 @@ function spamfree_contact_form($content) {
 			//$wpsf_contact_form_cc_to_name 		= $wpsf_contact_name;
 			$wpsf_contact_form_subject 			= '[Website Contact] '.$wpsf_contact_subject;
 			//$wpsf_contact_form_cc_subject		= '[Website Contact CC] '.$wpsf_contact_subject;
-			$wpsf_contact_form_msg_headers 		= "From: $wpsf_contact_name <$wpsf_contact_email>" . "\r\n" . "X-Mailer: PHP/" . phpversion();
+			$wpsf_contact_form_msg_headers 		= "From: $wpsf_contact_name <$wpsf_contact_email>" . "\r\n" . "Content-Type: text/plain\r\n";
+			// Another option: "Content-Type: text/html"
+			
 			// FORM INFO :: END
 			
 			// TEST TO PREVENT CONTACT FORM SPAM FROM BOTS :: BEGIN
@@ -412,7 +415,7 @@ function spamfree_contact_form($content) {
 					$wpsf_contact_form_msg_cc = $wpsf_contact_form_msg_1.$wpsf_contact_form_msg_3;
 					
 					// SEND MESSAGE
-					mail( $wpsf_contact_form_to, $wpsf_contact_form_subject, $wpsf_contact_form_msg, $wpsf_contact_form_msg_headers );
+					@wp_mail( $wpsf_contact_form_to, $wpsf_contact_form_subject, $wpsf_contact_form_msg, $wpsf_contact_form_msg_headers );
 										
 					$contact_response_status = 'thank-you';
 					
@@ -679,7 +682,7 @@ function spamfree_allowed_post($approved) {
 		
 function spamfree_denied_post($approved) {
 	// REJECT SPAM :: BEGIN
-	
+
 	// Update Count
 	update_option( 'spamfree_count', get_option('spamfree_count') + 1 );
 	// Akismet Accuracy Fix :: BEGIN
@@ -3887,7 +3890,7 @@ function spamfree_content_filter($commentdata) {
 	$spamfree_error_data = array( $spamfree_error_code, $blacklist_word_combo, $blacklist_word_combo_total );
 	
 	update_option( 'spamfree_error_data', $spamfree_error_data );
-	
+		
 	return $content_filter_status;
 	// CONTENT FILTERING :: END
 	}
@@ -4477,7 +4480,7 @@ if (!class_exists('wpSpamFree')) {
 		
 		function install_on_activation() {
 			global $wpdb;
-			$plugin_db_version = "1.9.8.2";
+			$plugin_db_version = "1.9.8.3";
 			$installed_ver = get_option('wp_spamfree_version');
 			$spamfree_options = get_option('spamfree_options');
 			//only run installation if not installed or if previous version installed
